addf81776b801bb659829d8bae84b60c81e87c2a
Oluwagbenga Bello <bellogbenga43@gmail.com>
2025-06-12 23:49:39 +0100

    added a feature to the controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as userRepo from "../repositories/userRepository";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (
   req: Request,
@@ -14,11 +15,53 @@ export const registerUser = async (
       return;
     }
 
-    const newUser = await userRepo.createUser({ name, email, password });
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: newUser });
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await userRepo.createUser({
+      name,
+      email,
+      password: hashPassword,
+    });
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userRepo.findUserByEmail(email);
+
+    if (!email) {
+      res.status(400).json({ message: "invalid email or password" });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ message: "invalid email or password" });
+    }
+    res.status(200).json({
+      message: "login successful",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "server error",
+      error,
+    });
   }
 };
