d2fcc82f27e57d4d6c75f1640b2c1ab53c5d274c
Oluwagbenga Bello <bellogbenga43@gmail.com>
2025-06-19 20:03:52 +0100

    updated server file

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import app from "./app";
-import { testConnection } from "./database/db";
+import db, { testConnection } from "./database/db";
 
 const PORT = process.env.PORT || 2000;
 
 const startServer = async () => {
   try {
     await testConnection();
+
+    // ✅ Run migrations on server start
+    await db.migrate.latest();
+    console.log("Migrations completed ✅");
+
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}`);
     });
   } catch (err) {
-    console.error("❌ Failed to connect to DB. Server not started.");
+    console.error(
+      "❌ Failed to connect to DB or run migration. Server not started.",
+      err
+    );
     process.exit(1);
   }
 };
