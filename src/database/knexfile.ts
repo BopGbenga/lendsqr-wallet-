7505a740fd9dde7330f93df01fd3ead5506e86ad
Oluwagbenga Bello <bellogbenga43@gmail.com>
2025-06-19 18:49:01 +0100

    updated the knexfile

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -18,6 +18,20 @@ const config: { [key: string]: Knex.Config } = {
       extension: "js",
     },
   },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+    },
+    migrations: {
+      directory: "./src/database/migrations",
+      extension: "js",
+    },
+  },
 };
 
 export default config;
